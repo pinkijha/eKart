6d60ca7d7ee4a696b69fea27a8a52b29a980f3a9
Pinki Jha <pinkijha1310@gmail.com>
2024-12-02 18:49:05 +0530

    add tailwind to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
-import './App.css'
+
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>Ekart</h1>
+        <h1 className='font-bold text-5xl text-blue-800 items-center
+        '>Ekart</h1>
       </div>
     </>
   )
