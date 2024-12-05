10679994309615c04bb44b7c7c81282bf5a15028
Pinki Jha <pinkijha1310@gmail.com>
2024-12-05 22:56:39 +0530

    add action & styled with tailwind css

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Header from './Header'
-import { P_IM2, P_IMG, P_IMG1 } from '../utils/Constant'
+import { P_IM2, P_IMG, P_IMG1 } from '../utils/Constant';
+import { FaPlusSquare } from "react-icons/fa";
+import { RiEdit2Fill } from "react-icons/ri";
+import { MdDelete } from "react-icons/md";
+import { FaEye } from "react-icons/fa";
 
 const Product = () => {
   const productList = [
@@ -11,7 +15,7 @@ const Product = () => {
       description: 'A plant-based gel cleanser that removes impurities while nourishing the skin with natural oils.',
       pImg : P_IMG1,
       category: 'Facial Cleansers',
-      price: '400.00 RS.',
+      price: '140.11',
     },
     {
       id: '2',
@@ -20,7 +24,7 @@ const Product = () => {
       description: 'Infused with rose oil, this cream provides intense hydration and protection for sensitive skin.',
       pImg : P_IMG,
       category: 'Moisturizers',
-      price: '700.00 RS.',
+      price: '170.22',
     },
     {
       id: '3',
@@ -29,30 +33,39 @@ const Product = () => {
       description: 'Infused with rose oil, this cream provides intense hydration and protection for sensitive skin.',
       pImg : P_IM2,
       category: 'Exfoliators',
-      price: '500.00 RS.',
+      price: '142.30',
     },
-  ]
+  ];
+   const thStyle = 'border border-gray-300 px-4 py-2';
+   const tdStyle = "border border-gray-300 px-4 py-2" ;
 
   return (
     
     <div>
       <div><Header/></div>
 
-      <div className=' md:mt-6 mt-2 flex flex-col items-center justify-center'>
+      <div className=' md:mt-7 mt-2 flex flex-col items-center justify-center'>
+        <div className='flex '>
         <h2 className='text-2xl font-bold text-blue-900 hover:scale-95 duration-200
          '>All Products List</h2>
-        <div>
+        </div>
+
+        <div className='relative'>
+        <button className='absolute -top-8 right-0  text-4xl text-orange-600'><FaPlusSquare /></button>
           {/* Table */}
           <table className="table-auto bg-white border md:my-4 border-gray-300 w-full max-w-4xl">
             <thead>
               <tr className="bg-gray-100 text-left">
-              <th className="border border-gray-300 px-4 py-2">Id</th>
-                <th className="border border-gray-300 px-4 py-2">Name</th>
-                <th className="border border-gray-300 px-4 py-2">Brand</th>
-                <th className="border border-gray-300 px-4 py-2">Description</th>
-                <th className="border border-gray-300 px-4 py-2">Image</th>
-                <th className="border border-gray-300 px-4 py-2">Category</th>
-                <th className="border border-gray-300 px-4 py-2">Price</th>
+              <th className="border border-gray-300 px-4 py-2">Id
+
+              </th>
+              <th className={thStyle}>Image</th>
+                <th className={thStyle}>Name</th>
+                <th className={thStyle}>Brand</th>
+                <th className={thStyle}>Description</th>
+                <th className={thStyle}>Category</th>
+                <th className={thStyle}>Price</th>
+                <th className={thStyle}>Action</th>
                 
               </tr>
             </thead>
@@ -63,19 +76,28 @@ const Product = () => {
                   key={product.id}
                   className="hover:bg-gray-50 duration-200 text-sm"
                 >
-                  <td className="border border-gray-300 px-4 py-2">{product.id}</td>
-                  <td className="border border-gray-300 px-4 py-2">{product.pName}</td>
-                  <td className="border border-gray-300 px-4 py-2">{product.bName}</td>
-                  <td className="border border-gray-300 px-4 py-2">{product.description}</td>
-                  <td className="border border-gray-300 px-4 py-2">
+                  <td className={tdStyle}>{product.id}</td>
+                  <td className={tdStyle}>
                     <img className='md:w-[200px]  border-gray-300 rounded-lg cursor-pointer
-                     hover:scale-150 duration-150' src={product.pImg} alt='Product-img' /></td>
-                  <td className="border border-gray-300 px-4 py-2">{product.category}</td>
-                  <td className="border border-gray-300 px-4 py-2">{product.price}</td>
-                  <td className="border border-gray-300 px-4 py-2"> 
-                  <button className='bg-green-600 text-white 
-                  font-semibold shadow-sm p-2 rounded-lg hover:scale-110 duration-200'>View
-                  </button></td>
+                     hover:scale-110 duration-150' src={product.pImg} alt='Product-img' /></td>
+                  <td className={tdStyle}>{product.pName}</td>
+                  <td className={tdStyle}>{product.bName}</td>
+                  <td className={tdStyle}>{product.description}</td>
+                  <td className={tdStyle}>{product.category}</td>
+                  <td className={tdStyle}>${product.price}</td>
+                  <td className={tdStyle}> 
+                 <span className='flex space-x-2'>
+                 <button className='text-green-600  text-2xl
+                  font-semibold shadow-sm p-1 rounded-lg hover:scale-110 duration-200'><FaEye />
+                  </button>
+                  <button className='text-blue-600  text-2xl
+                  font-semibold shadow-sm p-1 rounded-lg hover:scale-110 duration-200'><RiEdit2Fill />
+                  </button>
+                  <button className='text-red-600 text-2xl
+                  font-semibold shadow-sm p-1 rounded-lg hover:scale-110 duration-200'><MdDelete />
+                  </button>
+                 </span>
+                  </td>
                  
                 </tr>
               ))}
